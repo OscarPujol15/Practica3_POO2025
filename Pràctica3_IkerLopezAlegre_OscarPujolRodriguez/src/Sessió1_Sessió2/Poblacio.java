201aaa7d50758fecefa8702eba9eb6c11ce3ad86
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-02-15 00:49:27 +0100

    Update Poblacio.java

--- Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/Poblacio.java
+++ Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/Poblacio.java
@@ -1,5 +1,41 @@
 package Sessió1_Sessió2;
 
 public class Poblacio {
-
+	private String nom;
+	private ContenidorBrossa contenidor[];
+	private int numContenidors;
+	private int numContenidorsCarrer;
+	private final int INCREMENT;
+	
+	public Poblacio(String nom, int components , int increment) {
+		this.nom = nom;
+		this.contenidor = new ContenidorBrossa[components];
+		this.INCREMENT = increment;
+		this.numContenidors = 0;
+		this.numContenidorsCarrer=0;
+	}
+	
+	public String getNom() {return this.nom;}
+	public int getNumContenidors() {return this.numContenidors;}
+	public int getNumContenidorsCarrer() {return this.numContenidorsCarrer;}
+	
+	public ContenidorBrossa getContenidor(int quin) {
+		if (quin>this.numContenidors || quin<0) {
+			return null;
+		}
+		else {
+			return this.contenidor[quin];
+		}
+	}
+	
+	public void afegirContenidor(ContenidorBrossa p) {}
+	
+	public ContenidorBrossa mesRendiment(int tipus) {
+		for (int i =0; i<this.numContenidors; i++) {
+			if (this.contenidors[i].)) {
+				
+			}
+		}
+		
+	}
 }
