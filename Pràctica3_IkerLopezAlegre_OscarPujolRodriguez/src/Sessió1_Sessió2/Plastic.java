ed406b72996ef90e789d5055e333bdaad2a6157c
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-02-15 00:49:01 +0100

    Update Plastic.java

--- Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/Plastic.java
+++ Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/Plastic.java
@@ -1,5 +1,30 @@
 package Sessió1_Sessió2;
 
-public class Plastic {
+public class Plastic extends ContenidorBrossa {
 
+	private float reciclat;
+	
+	public Plastic (String codi, String ubicacio, int any, double TARA) {
+		super (codi, ContenidorBrossa.MARRO, ubicacio, any, TARA);
+		this.reciclat = 0;
+	}
+	
+	public Plastic (String codi, double TARA) {
+		this(codi, null, obtenirAnyActual(), TARA );
+	}
+	
+	public String getReciclat() {
+		return this.reciclat + " quilograms";
+	}
+	
+	public String toString() {
+		 return super.toString() + "Plastic reciclat : " + this.reciclat;
+	 }
+	
+	public void buidat (float pes) {
+		float kiloreci = (float) pes - (float) this.TARA;
+		kiloreci = kiloreci*(float)0.80;
+		this.reciclat += kiloreci;
+	}
 }
+
