159a2d669d8026feb32de2491f8609617967f4d4
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-02-22 11:53:49 +0100

    Update Rebuix.java

--- Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/Rebuix.java
+++ Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/Rebuix.java
@@ -1,25 +1,25 @@
 package Sessió1_Sessió2;
 
-public class Rebuig extends ContenidorBrossa{
-	private float reciclat;
-	
+	public class Rebuig extends ContenidorBrossa{
+		private float reciclat;
+
 	public Rebuig (String codi, String ubicacio, int any, double TARA) {
-		super (codi, ContenidorBrossa.MARRO, ubicacio, any, TARA);
+		super (codi, ContenidorBrossa.GRIS, ubicacio, any, TARA);
 		this.reciclat = 0;
 	}
-	
+
 	public Rebuig (String codi, double TARA) {
 		this(codi, null, obtenirAnyActual(), TARA );
 	}
-	
+
 	public String getReciclat() {
 		return this.reciclat + " tones";
 	}
-	
+
 	public String toString() {
-		 return super.toString() + "Rebuig reciclat : " + this.reciclat;
-	 }
-	
+		return super.toString() + "Rebuig reciclat : " + this.reciclat;
+	}
+
 	public void buidat (float pes) {
 		float kiloreci = (float) pes - (float) this.TARA;
 		kiloreci = kiloreci*(float)0.75;
@@ -27,4 +27,3 @@ public class Rebuig extends ContenidorBrossa{
 		this.reciclat += kiloreci;
 	}
 }
-
