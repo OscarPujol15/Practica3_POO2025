8ce8b2892d800745a31a59bca4dd3fb8fc5bdd0f
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-03-01 20:08:41 +0100

    Update ContenidorBrossa.java

--- Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/ContenidorBrossa.java
+++ Pràctica3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessió1_Sessió2/ContenidorBrossa.java
@@ -1,8 +1,8 @@
-package Sessió1_Sessió2;
+package Sessio3;
 
 import java.util.GregorianCalendar;
 
-public abstract class ContenidorBrossa implements Comparable {
+public abstract class ContenidorBrossa implements Comparable{
 	public final static int GROC = 0;
 	public final static int MARRO = 1;
 	public final static int GRIS = 2;
@@ -15,7 +15,12 @@ public abstract class ContenidorBrossa implements Comparable {
 	private int any; 
 	protected final double TARA; 
 
-	public ContenidorBrossa(String codi, int color, String ubicacio, int any, double TARA){
+	public ContenidorBrossa(String codi, int color, String ubicacio, int any, double TARA) throws IllegalArgumentException, NullPointerException{
+		if (color!=ContenidorBrossa.GROC && color!=ContenidorBrossa.MARRO && color!=ContenidorBrossa.GRIS && color!=ContenidorBrossa.GRIS &&
+				color!=ContenidorBrossa.BLAU &&color!=ContenidorBrossa.VERD) {throw new IllegalArgumentException("Valors Incorrectes");}
+		if (any != 0 && (any < 1000 || any > 9999)) {throw new IllegalArgumentException("Valors Incorrectes");}
+		if (codi == null) {throw new NullPointerException("Valor null");}
+		if (TARA<0) {throw new IllegalArgumentException("Valor incorrecte");}
 		this.codi = codi;
 		this.color = color; 
 		this.ubicacio = ubicacio;
@@ -34,7 +39,8 @@ public abstract class ContenidorBrossa implements Comparable {
 		int anyActual = avui.get(1);
 		return anyActual;
 	}
-	public String getTipusBrossa(){
+	
+	public String getTipusBrossa()throws IllegalArgumentException{
 		switch (this.color){
 		case 0:
 			return "Plastic";
@@ -47,14 +53,17 @@ public abstract class ContenidorBrossa implements Comparable {
 		case 4:
 			return "Vidre";
 		default: 
-			return "Error";
+			throw new IllegalArgumentException("Valor Incorrecte");
 		}
 	}
 
-	public void retirarViaPublica(){
+	public void retirarViaPublica()throws NullPointerException{
 		if (this.ubicacio!= null){
 			this.ubicacio = null;
 			this.any = 0;
+		}
+		else {
+			throw new NullPointerException("Valor null");
 		}
 	}
 
@@ -82,14 +91,12 @@ public abstract class ContenidorBrossa implements Comparable {
 		}
 	}
 
-	public String getEstat(){
-		int diferencia = 2025-this.any;
-		if (diferencia>5){
-			return "Vell";}
-		else if (diferencia < 3){
-			return "Nou";}
-		else{
-			return "Semi Nou";}
+	public String getEstat()throws IllegalArgumentException{
+		int diferencia = obtenirAnyActual()-this.any;
+		if (diferencia>5){return "Vell";}
+		else if (diferencia < 3 && diferencia> -1){return "Nou";}
+		else if (diferencia<0) {throw new IllegalArgumentException("Valor Incorrecte d'any");}
+		else{return "Semi Nou";}
 	}
 
 	public abstract void buidat (float pes);
@@ -102,31 +109,28 @@ public abstract class ContenidorBrossa implements Comparable {
 			return true;
 		}
 	}
-	
-	public int compareTo(Object c){
+
+	public int compareTo(Object c) throws ClassCastException{
 		ContenidorBrossa r;
 		if (! (c instanceof ContenidorBrossa)) {
-			return 32;
+			throw new ClassCastException("L'objecte no pertany a la classe ContenidorBrossa");
 		}
-		
-		else {
-			r = (ContenidorBrossa) c;
-			if (this.codi.compareTo(r.codi)<0) {return -1;}
-			else if (this.codi.compareTo(r.codi)>0) {return 1;}
-			else {return 0;}
-		}
-		
+		r = (ContenidorBrossa) c;
+		return this.codi.compareTo(r.codi);
 	}
-	
-	
-	public String ToString () {
+
+
+	public String toString () {
 		String ubicacions;
 		if (this.ubicacio!=null){
-			ubicacions = this.ubicacio;}
+			ubicacions = this.ubicacio;
+			}
 		else{
-			ubicacions = "retirat";}
-		
+			ubicacions = "retirat";
+			}
+
 		return "Codi: " + this.codi + "Color: " + this.getTipusBrossa() + 
 				"Ubicació:" + ubicacions + "l’Ajuntament Tara:" + Math.round((this.TARA*100)/100);
-	}
+	}	
+
 }
