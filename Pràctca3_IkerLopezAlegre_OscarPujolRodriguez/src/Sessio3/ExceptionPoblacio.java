9b645985777c4452b705927773e79106b9501a25
OscarPujol15 <opujolr@edu.tecnocampus.cat>
2025-03-01 20:10:43 +0100

    Add files via upload

---
+++ Pràctca3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessio3/ExceptionPoblacio.java
@@ -0,0 +1,8 @@
+package Sessio3;
+
+public class ExceptionPoblacio extends Exception {
+	
+	public ExceptionPoblacio(String msg) {
+		super(msg);
+	}
+}
---
+++ Pràctca3_IkerLopezAlegre_OscarPujolRodriguez/src/Sessio3/Organic.java
@@ -0,0 +1,30 @@
+package Sessio3;
+
+public class Organic extends ContenidorBrossa{
+	
+	private float reciclat;
+	
+	public Organic (String codi, String ubicacio, int any, double TARA) {
+		super (codi, ContenidorBrossa.MARRO, ubicacio, any, TARA);
+		this.reciclat = 0;
+	}
+	
+	public Organic (String codi, double TARA) {
+		this(codi, null, obtenirAnyActual(), TARA );
+	}
+	
+	public String getReciclat() {
+		return this.reciclat + "tones";
+	}
+	
+	 public String toString() {
+		 return super.toString() + "Organic reciclat : " + this.reciclat;
+	 }
+	 
+		public void buidat (float pes) {
+			float kiloreci = (float) pes - (float) this.TARA;
+			kiloreci = kiloreci*(float)0.90;
+			kiloreci = kiloreci/1000;
+			this.reciclat += kiloreci;
+		}
+}
